f1b734fb044dd8df4d6f1175d1e356694cb06d50
Rajkumar <rajkmr2001@gmail.com>
2025-08-15 22:39:04 +0530

    Add qno_1 and qno_2

---
+++ qno_1.py
@@ -0,0 +1,7 @@
+"""
+Write a program that asks your name and age, then prints:
+"Hello <name>, you are <age> years old."
+"""
+name = input('Enter your name :  ')
+age  = input('Enter your age  :  ')
+print( f'hello {name}, you are {age} years old.')
