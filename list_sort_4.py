5c63f2b09b870a05aa4980ebd0ff8daa77d4c718
Rajkumar <rajkmr2001@gmail.com>
2025-08-26 22:39:21 +0530

    new commit

--- list_sort_4.py
+++ list_sort_4.py
@@ -1,4 +1,4 @@
-#write a program to accept marks of 6 students and display them in a sorted manner
+#write a program to accept marks of 6 students and display them in a sorted manner.
 marks = []
 for i in range(6):
     data = int(input(f"Enter the marks{i +1}:  "))
